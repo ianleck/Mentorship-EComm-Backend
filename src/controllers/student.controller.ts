980e813200434ee62bf2a92d087be1fceda989b5
kengthong <wookengthong@gmail.com>
2021-02-18 22:32:20 +0800

    refactor: refactored update student authorization checks

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -2,6 +2,7 @@ import StudentService from '../services/student.service';
 import httpStatusCodes from 'http-status-codes';
 import apiResponse from '../utilities/apiResponse';
 import logger from '../config/logger';
+import { USER_TYPE_ENUM_OPTIONS } from '../constants/enum';
 
 export class StudentController {
   // public static async createStudent(req, res) {
@@ -11,13 +12,28 @@ export class StudentController {
   //         apiResponse.result(res, {message: 'success'}, httpStatusCodes.OK);
   //     } catch(e) {
   //         logger.error('[studentController.updateStudent]' + e.toString());
-  //         return apiResponse.error(res, 400, {message: e.toString()});
+  //         return apiResponse.error(res, httpStatusCodes.BAD_REQUEST, {message: e.toString()});
   //     }
   // }
 
   public static async updateStudent(req, res) {
+    const { user } = req;
     const { accountId } = req.query;
     const { student } = req.body;
+
+    console.log('user =', user);
+    console.log('accountId =', accountId);
+    console.log('user.accountId == accountId =', user.accountId == accountId);
+    // check if user is updating his/her own account or if it is an admin who is updating the account
+    if (
+      user.accountId != accountId ||
+      user.userType == USER_TYPE_ENUM_OPTIONS.ADMIN
+    ) {
+      return apiResponse.error(res, httpStatusCodes.UNAUTHORIZED, {
+        message: httpStatusCodes.getStatusText(httpStatusCodes.UNAUTHORIZED),
+      });
+    } // end check
+
     try {
       const user = await StudentService.updateStudent(accountId, student);
       apiResponse.result(
@@ -27,7 +43,9 @@ export class StudentController {
       );
     } catch (e) {
       logger.error('[studentController.updateStudent]' + e.toString());
-      return apiResponse.error(res, 400, { message: e.toString() });
+      return apiResponse.error(res, httpStatusCodes.BAD_REQUEST, {
+        message: e.toString(),
+      });
     }
   }
 }
