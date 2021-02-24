00dc83858ca891f6844d55511a9346d67c9443c8
kengthong <wookengthong@gmail.com>
2021-02-24 21:54:23 +0800

    added seed scripts. Update package.json for new commands, updated Readme

--- src/migrations/20210224120816-admin-with-constraint.js
+++ src/migrations/20210224120816-admin-with-constraint.js
@@ -8,59 +8,85 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    return queryInterface.createTable('Admin', {
-      accountId: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-      },
-      username: {
-        type: Sequelize.STRING,
-        unique: true,
-      },
-      password: {
-        type: Sequelize.STRING,
-      },
-      firstName: {
-        type: Sequelize.STRING,
-      },
-      lastName: {
-        type: Sequelize.STRING,
-      },
-      emailVerified: {
-        type: Sequelize.BOOLEAN,
-      },
-      email: {
-        type: Sequelize.STRING,
-      },
-      contactNumber: {
-        type: Sequelize.STRING,
-      },
-      status: {
-        type: Sequelize.ENUM('ACTIVE', 'BANNED'),
-        defaultValue: 'ACTIVE',
-      },
+    return queryInterface.sequelize.transaction(async (t) => {
+      return await queryInterface
+        .createTable('Admin', {
+          accountId: {
+            allowNull: false,
+            primaryKey: true,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+          },
+          username: {
+            type: Sequelize.STRING,
+            unique: true,
+          },
+          password: {
+            type: Sequelize.STRING,
+          },
+          firstName: {
+            type: Sequelize.STRING,
+          },
+          lastName: {
+            type: Sequelize.STRING,
+          },
+          emailVerified: {
+            type: Sequelize.BOOLEAN,
+          },
+          email: {
+            type: Sequelize.STRING,
+          },
+          contactNumber: {
+            type: Sequelize.STRING,
+          },
+          status: {
+            type: Sequelize.ENUM('ACTIVE', 'BANNED'),
+            defaultValue: 'ACTIVE',
+          },
 
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      deletedAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      userType: {
-        type: Sequelize.ENUM('STUDENT', 'SENSEI', 'ADMIN'),
-      },
-      permission: {
-        type: Sequelize.ENUM('ADMIN', 'SUPERADMIN'),
-        defaultValue: 'ADMIN',
-      },
+          createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.fn('NOW'),
+          },
+          updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.fn('NOW'),
+          },
+          deletedAt: {
+            allowNull: true,
+            type: Sequelize.DATE,
+          },
+          userType: {
+            type: Sequelize.ENUM('STUDENT', 'SENSEI', 'ADMIN'),
+          },
+          permission: {
+            type: Sequelize.ENUM('ADMIN', 'SUPERADMIN'),
+            defaultValue: 'ADMIN',
+          },
+          updatedBy: {
+            type: Sequelize.UUID,
+          },
+        })
+        .then(async () => {
+          return await queryInterface.addConstraint(
+            'Admin',
+            {
+              type: 'foreign key',
+              fields: ['updatedBy'],
+              references: {
+                table: 'Admin',
+                field: 'accountId',
+              },
+              onDelete: 'no action',
+              onUpdate: 'no action',
+            },
+            {
+              transaction: t,
+            }
+          );
+        });
     });
   },
 
