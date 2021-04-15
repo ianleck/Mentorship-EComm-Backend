62a7b3eb5d93f6819f876332b2aecc7b4f58dfd3
Ian <39756111+ianleck@users.noreply.github.com>
2021-04-15 13:25:16 +0800

    add env changes

--- src/config/db/db-config.ts
+++ src/config/db/db-config.ts
@@ -1,41 +1,44 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 // dotenv.config({path: `../../../env/.env.${process.env.NODE_ENV}`});
-dotenv.config({path: `./env/.env.${process.env.NODE_ENV}`});
+dotenv.config({ path: `./env/.env.${process.env.NODE_ENV}` });
 export default {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_Dialect,
-        define: {
-            freezeTableName: true,
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
-        }
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_Dialect,
+    define: {
+      freezeTableName: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
     },
-    test: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_Dialect,
-        define: {
-            freezeTableName: true,
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
-        }
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_Dialect,
+    define: {
+      freezeTableName: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
     },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_Dialect,
-        define: {
-            freezeTableName: true,
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
-        }
-    }
-}
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_Dialect,
+    define: {
+      freezeTableName: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+};
